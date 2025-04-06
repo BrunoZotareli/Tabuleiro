81622e740e4c57bba42179d799681245bf7ef4ec
BrunoZotareli <zotarelibruno@gmail.com>
2025-04-06 00:39:24 -0300

    tabuleiro que move

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function moveP1casa(){
+    let tabuleiro = document.getElementById('tabuleiro')
+    tabuleiro.src = 'imagens/e-5.png'
+    }
+    
+    function moveP2casa(){
+    let tabuleiro = document.getElementById('tabuleiro')
+    tabuleiro.src = 'imagens/e-6.png'
+    }
+    
+    
+    function moveCesquerda(){
+      let tabuleiro = document.getElementById('tabuleiro')
+     tabuleiro.src = 'imagens/C-g-4-f.png' 
+    }
+    
+    function moveCdireita(){
+      let tabuleiro = document.getElementById('tabuleiro')
+      tabuleiro.src = 'imagens/C-g-4-H.png' 
+    }
